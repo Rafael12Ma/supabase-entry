9fe475acba042e08a2b08dd847453a9fe6a21c29
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2026-01-09 15:10:55 +0200

    First commit of supabase app

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center text-white bg-black font-sans dark:bg-black">
+      <h1>Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
